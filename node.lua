4ae4ab84141896d69def5a8621f8a38ca9e0d599
samuelchovanec <samuel.chovanec@gmail.com>
2023-11-10 22:12:53 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -3,6 +3,10 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 util.no_globals()
 
 local on = false
+local video = resource.load_video{
+    file = "video.mp4";
+    looped = true;
+}
 
 util.data_mapper{
     state = function(state)
@@ -12,7 +16,7 @@ util.data_mapper{
 
 function node.render()
     if on then
-        video1:draw(0, 0, WIDTH, HEIGHT)
+        video:draw(0, 0, WIDTH, HEIGHT)
     else
         gl.clear(1, 0, 0, 1) -- red
     end
