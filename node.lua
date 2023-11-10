8fc4b005c5fc43d86e9874f69df7b5a53b4376fc
samuelchovanec <samuel.chovanec@gmail.com>
2023-11-10 22:53:58 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -2,13 +2,13 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 util.no_globals()
 
-export INFOBEAMER_AUDIO_TARGET=local
+
 
 local on = false
 local video = resource.load_video{
-    file = "video.mp4";
-    looped = false;
-    audio = true;
+    file = "video.mp4",
+    looped = false,
+    audio = true,
 }
 
 util.data_mapper{
