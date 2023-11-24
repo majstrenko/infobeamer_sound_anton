0f1088cbeab0d4b7fcb05e2bcb45ddafa8817fd6
samuelchovanec <samuel.chovanec@gmail.com>
2023-11-24 20:59:04 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -3,42 +3,66 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 util.no_globals()
 
 local videos = {
-    [16] = resource.load_video{file = "1.mp4", looped = false, audio = true},
-    [17] = resource.load_video{file = "2.mp4", looped = false, audio = true},
-    [18] = resource.load_video{file = "3.mp4", looped = false, audio = true},
-    [19] = resource.load_video{file = "4.mp4", looped = false, audio = true},
+    [16] = resource.load_video{file = "1.mp4", looped = false, audio = true, paused = true},
+    [17] = resource.load_video{file = "2.mp4", looped = false, audio = true, paused = true},
+    [18] = resource.load_video{file = "3.mp4", looped = false, audio = true, paused = true},
+    [19] = resource.load_video{file = "4.mp4", looped = false, audio = true, paused = true},
 }
 
 local current_video = nil
 local video_playing = false
 
 util.data_mapper{
-    ["state/16:1"] = function(state)
+    ["state/16"] = function(state)
+        print("Received state for pin 16: " .. state)
         if state == '1' then
+            if current_video then
+                current_video:stop()
+                print("Stopping current video")
+            end
             current_video = videos[16]
             current_video:start()
             video_playing = true
+            print("Starting video for pin 16")
         end
     end,
     ["state/17"] = function(state)
+        print("Received state for pin 17: " .. state)
         if state == '1' then
+            if current_video then
+                current_video:stop()
+                print("Stopping current video")
+            end
             current_video = videos[17]
             current_video:start()
             video_playing = true
+            print("Starting video for pin 17")
         end
     end,
     ["state/18"] = function(state)
+        print("Received state for pin 18: " .. state)
         if state == '1' then
+            if current_video then
+                current_video:stop()
+                print("Stopping current video")
+            end
             current_video = videos[18]
             current_video:start()
             video_playing = true
+            print("Starting video for pin 18")
         end
     end,
     ["state/19"] = function(state)
+        print("Received state for pin 19: " .. state)
         if state == '1' then
+            if current_video then
+                current_video:stop()
+                print("Stopping current video")
+            end
             current_video = videos[19]
             current_video:start()
             video_playing = true
+            print("Starting video for pin 19")
         end
     end,
 }
@@ -47,6 +71,7 @@ function node.render()
     if video_playing and current_video then
         local state, w, h = current_video:state()
         if state == "finished" then
+            print("Video finished playing")
             video_playing = false
             current_video = nil
             gl.clear(1, 0, 0, 1) -- red, default state
