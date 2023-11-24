8122da09f45e1b722645942666ff73521b51adf8
samuelchovanec <samuel.chovanec@gmail.com>
2023-11-24 22:17:21 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -14,26 +14,25 @@ local video_playing = false
 util.data_mapper{
     ["state/16"] = function(state)
         if state == 'toggle' then
-            video:stop() -- Stop the video if it's playing
-            video:start() -- Start the video from the beginning
-            video_playing = true
-            print("Toggling video for pin 16")
+            if not video_playing then
+                video:start() -- Start the video from the beginning
+                video_playing = true
+                print("Starting video for pin 16")
+            end
         end
     end
-    -- Add similar handlers for other pins if needed
 }
 
 function node.render()
     if video_playing then
         local state, w, h = video:state()
         if state == "finished" then
-            print("Video finished playing")
             video_playing = false
-            gl.clear(1, 0, 0, 1) -- red, default state
+            video:stop()
+            print("Video finished playing")
         else
             video:draw(0, 0, WIDTH, HEIGHT)
         end
-    else
-        gl.clear(1, 0, 0, 1) -- red, default state
     end
+    -- When video is not playing, the screen remains empty
 end
