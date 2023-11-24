f4107b368c66768b47e8a2cf24841d16175efaac
samuelchovanec <samuel.chovanec@gmail.com>
2023-11-24 22:49:05 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -2,42 +2,58 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 util.no_globals()
 
-local video1 -- Video to be played when 'on' is true
-local video2 -- Video to be played when 'on' is false
-local on = false
+local videos = {
+    [16] = resource.load_video{file = "video_for_pin_16.mp4", looped = false, audio = true},
+    [17] = resource.load_video{file = "video_for_pin_17.mp4", looped = false, audio = true},
+    [18] = resource.load_video{file = "video_for_pin_18.mp4", looped = false, audio = true},
+    [19] = resource.load_video{file = "video_for_pin_19.mp4", looped = false, audio = true},
+}
 
-function load_videos()
-    video1 = resource.load_video{
-        file = "1.mp4",
-        looped = true,
-        paused = true,
-    }
-    video2 = resource.load_video{
-        file = "2.mp4",
-        looped = true,
-        paused = true,
-    }
-end
+local current_video = nil
+local video_playing = false
 
 util.data_mapper{
-    state = function(state)
-        on = state == '1'
-        if on then
-            video1:start()
-            video2:stop()
-        else
-            video1:stop()
-            video2:start()
+    ["state/16"] = function(state)
+        if state == '1' then
+            current_video = videos[16]
+            current_video:start()
+            video_playing = true
+        end
+    end,
+    ["state/17"] = function(state)
+        if state == '1' then
+            current_video = videos[17]
+            current_video:start()
+            video_playing = true
+        end
+    end,
+    ["state/18"] = function(state)
+        if state == '1' then
+            current_video = videos[18]
+            current_video:start()
+            video_playing = true
+        end
+    end,
+    ["state/19"] = function(state)
+        if state == '1' then
+            current_video = videos[19]
+            current_video:start()
+            video_playing = true
         end
     end,
 }
 
 function node.render()
-    if on then
-        video1:draw(0, 0, WIDTH, HEIGHT)
+    if video_playing and current_video then
+        local state, w, h = current_video:state()
+        if state == "finished" then
+            video_playing = false
+            current_video = nil
+            gl.clear(1, 0, 0, 1) -- red, default state
+        else
+            current_video:draw(0, 0, WIDTH, HEIGHT)
+        end
     else
-        video2:draw(0, 0, WIDTH, HEIGHT)
+        gl.clear(1, 0, 0, 1) -- red, default state
     end
 end
-
-load_videos()
