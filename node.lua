baf3437408c7b2d98a6baf63e18a907904953aa6
samuelchovanec <samuel.chovanec@gmail.com>
2023-11-24 22:12:12 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -9,21 +9,30 @@ local video = resource.load_video{
     paused = true
 }
 
+local video_playing = false
+
 util.data_mapper{
     ["state/16"] = function(state)
-        if state == 'restart' then
+        if state == 'toggle' then
             video:stop() -- Stop the video if it's playing
             video:start() -- Start the video from the beginning
-            print("Restarting video for pin 16")
+            video_playing = true
+            print("Toggling video for pin 16")
         end
     end
     -- Add similar handlers for other pins if needed
 }
 
 function node.render()
-    local video_state, w, h = video:state()
-    if video_state ~= "finished" then
-        video:draw(0, 0, WIDTH, HEIGHT)
+    if video_playing then
+        local state, w, h = video:state()
+        if state == "finished" then
+            print("Video finished playing")
+            video_playing = false
+            gl.clear(1, 0, 0, 1) -- red, default state
+        else
+            video:draw(0, 0, WIDTH, HEIGHT)
+        end
     else
         gl.clear(1, 0, 0, 1) -- red, default state
     end
