f95ca0ca635ffafbf75732619dbf715fcbb30ad9
majstrenko <142303133+majstrenko@users.noreply.github.com>
2023-12-01 14:19:05 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -4,33 +4,45 @@ util.no_globals()
 
 local current_video = nil
 local video_playing = false
-
+local video_files = {
+    [16] = "1.mp4",
+    [17] = "2.mp4",
+    [18] = "3.mp4",
+    [19] = "4.mp4",
+}
 
 local function start_video(pin)
-         local video = {
-                  [16] = resource.load_video{file = "1.mp4"; looped = false; audio = true; paused = true},
-                  [17] = resource.load_video{file = "2.mp4"; looped = false; audio = true; paused = true},
-                  [18] = resource.load_video{file = "3.mp4"; looped = false; audio = true; paused = true},
-                  [19] = resource.load_video{file = "4.mp4"; looped = false; audio = true; paused = true},
-  }
-        
-        if current_video then
-                current_video:dispose()
-        end
-        current_video = video[pin]
+    if current_video and video_playing then
+        -- Logic to handle when a video is already playing (if needed)
+        -- E.g., you can queue the next video or immediately switch to it
+        current_video:dispose()
+        collectgarbage()  -- Explicit garbage collection
+    end
+
+    local video_file = video_files[pin]
+    if video_file then
+        current_video = resource.load_video{
+            file = video_file,
+            looped = false,
+            audio = true,
+            paused = true
+        }
         current_video:start()
         video_playing = true
+    else
+        -- Handle case when no video is associated with the pin
+        -- E.g., log an error or perform some default action
+    end
 end
-
 
 local function stop_video()
     if current_video then
         current_video:dispose()
+        collectgarbage()  -- Explicit garbage collection
     end
-        current_video = nil
-        video_playing = false
+    current_video = nil
+    video_playing = false
 end
-
 
 util.data_mapper{
     ["state/16"] = function(state)
